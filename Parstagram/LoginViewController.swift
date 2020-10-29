0bc3138b178de03e50be7fa993e2491f7aee1bf1
Pranav Rakkappan <59074909+Superveloce750-4@users.noreply.github.com>
2020-10-28 23:54:55 -0400

    Parstagram

--- Parstagram/LoginViewController.swift
+++ Parstagram/LoginViewController.swift
@@ -11,8 +11,8 @@ import Parse
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var usernameField: UITextField!
-    
     @IBOutlet weak var passwordField: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,24 +20,40 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func onSignUp(_ sender: Any) {
+    @IBAction func onSignin(_ sender: Any) {
+        
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if (user != nil){
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+            else{
+                print("Error")
+            }
+        }
+       
+        
+        
+    }
+    
+    
+    @IBAction func onSignup(_ sender: Any) {
+        
         let user = PFUser()
         user.username = usernameField.text
         user.password = passwordField.text
-        
-        user.signUpInBackground { (success, error) in
-            if error != nil {
+        user.signUpInBackground { (sucess, error) in
+            if sucess{
                 self.performSegue(withIdentifier: "loginSegue", sender: nil)
-            } else {
-                print("Error: \(error?.localizedDescription)")
+        }
+            else{
+                print("Error: \(error?.localizedDescription) ")
             }
+            
         }
     }
-    
-    @IBAction func onSignIn(_ sender: Any) {
-        
-    }
-    
     
     /*
     // MARK: - Navigation
